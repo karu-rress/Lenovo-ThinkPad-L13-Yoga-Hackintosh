0a5fded2bd78224e24367c629bab6e0b3c9b7e29
karu <32568363+karu-rress@users.noreply.github.com>
2024-08-24 21:56:29 +0900

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,3 +2,41 @@
 sudo diskutil mount /dev/disk0s1
 
 cd /Volumes/EFI/EFI/OC
+
+<key>Misc</key>
+	<dict>
+		<key>BlessOverride</key>
+		<array/>
+		<key>Boot</key>
+		<dict>
+			<key>ConsoleAttributes</key>
+			<integer>0</integer>
+			<key>HibernateMode</key>
+			<string>None</string>
+			<key>HibernateSkipsPicker</key>
+			<false/>
+			<key>HideAuxiliary</key>
+			<true/>
+			<key>InstanceIdentifier</key>
+			<string></string>
+			<key>LauncherOption</key>
+			<string>Disabled</string>
+			<key>LauncherPath</key>
+			<string>Default</string>
+			<key>PickerAttributes</key>
+			<integer>17</integer>
+			<key>PickerAudioAssist</key>
+			<false/>
+			<key>PickerMode</key>
+			<string>External</string>
+			<key>PickerVariant</key>
+			<string>Acidanthera\GoldenGate</string>
+			<key>PollAppleHotKeys</key>
+			<false/>
+			<key>ShowPicker</key>
+			<true/>
+			<key>TakeoffDelay</key>
+			<integer>0</integer>
+			<key>Timeout</key>
+			<integer>3</integer>
+		</dict>
